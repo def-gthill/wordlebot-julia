8c66e64850405bee77e7ed7e7a535f902bbc66b3
Graham Hill <gthill97@gmail.com>
2022-01-24 23:50:28 -0700

    Getting clues

--- src/Wordlebot.jl
+++ src/Wordlebot.jl
@@ -1,7 +1,52 @@
 module Wordlebot
 
-export cluefor_text
+export cluefor, cluefor_text
 
-cluefor_text(guess::AbstractString, target::AbstractString) = "_____"
+const CLUE_CHARS = "_?\$"
+
+function cluefor(guess::AbstractString, target::AbstractString)::Vector{Int8}
+    result = zeros(Int8, length(target))
+    unmatched_indices = BitSet(keys(target))
+    mark_strong_matches!(result, guess, target, unmatched_indices)
+    mark_weak_matches!(result, guess, target, unmatched_indices)
+    result
+end
+
+function mark_strong_matches!(result, guess, target, unmatched_indices)
+    for (i, char) in pairs(guess)
+        if char == target[i]
+            result[i] = 2
+            delete!(unmatched_indices, i)
+        end
+    end
+end
+
+function mark_weak_matches!(result, guess, target, unmatched_indices)
+    for (i, char) in pairs(guess)
+        if result[i] > 0 continue end
+        matching_index = first_or_nothing(unmatched_indices) do j
+            char == target[j]
+        end
+        if matching_index !== nothing
+            result[i] = 1
+            delete!(unmatched_indices, matching_index)
+        end
+    end
+end
+
+function first_or_nothing(f, c)
+    for elem in c
+        if f(elem)
+            return elem
+        end
+    end
+end
+
+function cluefor_text(guess::AbstractString, target::AbstractString)::String
+    clue_chars = map(cluefor(guess, target)) do i
+        CLUE_CHARS[i + 1]
+    end
+    String(clue_chars)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,27 @@ using Wordlebot
 
 @testset "cluefor" begin
     @test cluefor_text("looks", "panic") == "_____"
+
+    @test cluefor_text("tries", "panic") == "__?__"
+    @test cluefor_text("scrap", "craps") == "?????"
+
+    @test cluefor_text("remit", "panic") == "___\$_"
+    @test cluefor_text("trips", "tries") == "\$\$\$_\$"
+
+    @test cluefor_text("strip", "panic") == "___\$?"
+    @test cluefor_text("canid", "panic") == "?\$\$\$_"
+
+    @test cluefor_text("petty", "tangy") == "__?_\$"
+    @test cluefor_text("petty", "tenet") == "_\$??_"
+
+    @test cluefor_text("petty", "litre") == "_?\$__"
+    @test cluefor_text("litre", "petty") == "__\$_?"
+
+    @test cluefor_text("petty", "total") == "__\$?_"
+    @test cluefor_text("total", "petty") == "?_\$__"
+    @test cluefor_text("petty", "taste") == "_??\$_"
+    @test cluefor_text("taste", "petty") == "?__\$?"
+
+    @test cluefor("looks", "panic") == zeros(Int8, 5)
+    @test cluefor("scrap", "craps") == ones(Int8, 5)
 end
